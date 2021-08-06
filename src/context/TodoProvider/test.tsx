8d4aefaae6d9fbb67f77fa06bfda2daf4cc40b47
Franklyn Tique Grajales <franktique@gmail.com>
2021-08-06 15:19:57 -0500

    tets unit added

--- src/context/TodoProvider/test.tsx
+++ src/context/TodoProvider/test.tsx
@@ -1,41 +1,42 @@
-import * as provider from ".";
+import { useTodo, TodoProvider } from ".";
 import TodoList from "../../components/TodoList";
 import { StyledAppContainer } from "../../components/AppContainer/Styled";
-import { TodoContextType } from "../../types";
-import { render } from "@testing-library/react";
+import { Todo } from "../../types";
+import { render, waitFor } from "@testing-library/react";
 
-const todos: TodoContextType = {
-  todos: [
-    {
-      id: 1,
-      description: "sample todo",
-      dueDate: new Date(
-        "Tue Aug 3 2021 00:00:00 GMT-0700 (Pacific Daylight Time)"
-      ),
-      isComplete: false,
-      overDue: true,
-    },
-    {
-      id: 2,
-      description: "sample todo2",
-      dueDate: new Date(
-        "Tue Aug 13 2021 00:00:00 GMT-0700 (Pacific Daylight Time)"
-      ),
-      isComplete: true,
-      overDue: false,
-    },
-  ],
-  updateTodo: false,
-};
+const getTodoListMock = jest.fn();
+getTodoListMock.mockImplementation(
+  (): Promise<Todo[]> => Promise.resolve(todos)
+);
+jest.mock("../../api/TodoService", () => {
+  return jest.fn().mockImplementation(() => {
+    return { getTodoList: getTodoListMock };
+  });
+});
 
-const useTodoMock = jest.spyOn(provider, "useTodo");
-
-useTodoMock.mockImplementation(() => todos);
-
-const { useTodo, TodoProvider } = provider;
+const todos: Todo[] = [
+  {
+    id: 1,
+    description: "sample todo",
+    dueDate: new Date(
+      "Tue Aug 3 2021 00:00:00 GMT-0700 (Pacific Daylight Time)"
+    ),
+    isComplete: false,
+    overDue: true,
+  },
+  {
+    id: 2,
+    description: "sample todo2",
+    dueDate: new Date(
+      "Tue Aug 13 2021 00:00:00 GMT-0700 (Pacific Daylight Time)"
+    ),
+    isComplete: true,
+    overDue: false,
+  },
+];
 
 describe("TodoProvider", () => {
-  it("renders with mock data", () => {
+  it("renders with mock data", async () => {
     const { getByTestId } = render(
       <TodoProvider>
         <StyledAppContainer>
@@ -43,10 +44,12 @@ describe("TodoProvider", () => {
         </StyledAppContainer>
       </TodoProvider>
     );
-    const itemOneDescription = getByTestId("todo-description-1");
-    expect(itemOneDescription.textContent).toEqual("sample todo");
+    await waitFor(() => {
+      const itemOneDescription = getByTestId("todo-description-1");
+      expect(itemOneDescription.textContent).toEqual("sample todo");
 
-    const itemTwoDescription = getByTestId("todo-description-2");
-    expect(itemTwoDescription.textContent).toEqual("sample todo2");
+      const itemTwoDescription = getByTestId("todo-description-2");
+      expect(itemTwoDescription.textContent).toEqual("sample todo2");
+    });
   });
 });
